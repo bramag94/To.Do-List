78d86ab02662b6efdee08adb5f2b2db8179c0478
bramag94 <bramag94@hotmail.com>
2021-03-30 19:11:31 +0000

    con BS

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,38 +5,57 @@ export function Home() {
 	const [tareas, setTareas] = useState("");
 	const [lista, setLista] = useState([]);
 	return (
-		<div className="text-center mt-5">
-			<h1>TO DO LIST </h1>
+		<div className="bg-dark">
+			<div
+				className="card text-center mt-5 bg-dark"
+				style={{ width: "18rem;" }}>
+				<div className="card body">
+					<h1 className="card-title">TO DO LIST </h1>
 
-			<input
-				type="text"
-				placeholder="Ingrese sus datos"
-				onChange={e => setTareas(e.target.value)}
-				value={tareas}
-				onKeyPress={e => {
-					if (e.key == "Enter") {
-						setLista(lista.concat(tareas));
-						setTareas("");
-						console.log({ lista });
-					}
-				}}
-			/>
-			{lista.map((item, index) => {
-				return (
-					<li
-						onDoubleClick={() => {
-							setLista(
-								lista.filter(
-									(itemf, indexf) => indexf !== index
-								)
+					<h6 className="card-subtitle mb-2 text-muted">
+						<strong>Instucciones: </strong>
+						Agregue sus tareas en la caja correspondiente y presione
+						Enter. En caso de error, de doble click sobre la tarea
+						para eliminarla.
+					</h6>
+
+					<ul className="list-group list-group-flush">
+						<il>
+							<input
+								type="text"
+								placeholder="Ingrese sus datos"
+								onChange={e => setTareas(e.target.value)}
+								value={tareas}
+								onKeyPress={e => {
+									if (e.key == "Enter") {
+										setLista(lista.concat(tareas));
+										setTareas("");
+										console.log({ lista });
+									}
+								}}
+							/>{" "}
+						</il>
+
+						{lista.map((item, index) => {
+							return (
+								<li
+									onDoubleClick={() => {
+										setLista(
+											lista.filter(
+												(itemf, indexf) =>
+													indexf !== index
+											)
+										);
+									}}
+									key={index}>
+									{" "}
+									{item}
+								</li>
 							);
-						}}
-						key={index}>
-						{" "}
-						{item}
-					</li>
-				);
-			})}
+						})}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
